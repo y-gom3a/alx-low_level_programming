fe5be5454493a2747ba628eb84db3e170dbdf6cc
y-gom3a <yosifgomaa2002@gmail.com>
2023-03-17 04:11:24 -0700

    hghg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - this is the main function 
- * 
- * Return: always 0
- */
+* main - this is the main function 
+* 
+* Return: always 0
+*/
 int main(void)
 {	
 puts("\"Programming is like building a multilingual puzzle");
