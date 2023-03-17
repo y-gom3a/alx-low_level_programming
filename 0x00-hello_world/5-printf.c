1bb3d0cd8816c796cbec773490353bf90882fc83
y-gom3a <yosifgomaa2002@gmail.com>
2023-03-17 04:08:05 -0700

    hghg

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - this is the main function 
  *
  * Return: 0 when succesful
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
