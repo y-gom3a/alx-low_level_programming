6d7e960f969c12943fd79fe75a670ce549516d9f
y-gom3a <yosifgomaa2002@gmail.com>
2023-03-16 19:32:31 -0700

    love

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - this is the main function 
  * 
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	puts("\"programing is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
