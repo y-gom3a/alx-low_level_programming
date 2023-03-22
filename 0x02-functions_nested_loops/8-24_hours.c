5e6cc899c0ea0b7862adc0b6385afd16968cd486
y-gom3a <yosifgomaa2002@gmail.com>
2023-03-21 18:33:13 -0700

    fg

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - main functin timer
+ *
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+int a, b, c, d;
+for (a = 0 ; a <= 2 ; a++)
+{
+for (b = 0 ; b <= 9 ; b++)
+{
+for (c = 0 ; c <= 5 ; c++)
+{
+for (d = 0 ; d <= 9 ; d++)
+{
+if (a >= 2 && b >= 4)
+break;
+_putchar(a + 48);
+-putchar(b + 48);
+-putchar (58);
+-putchar(c + 48);
+_putchar(d + 48);
+_putchar('\n');
+}
+}
+}
