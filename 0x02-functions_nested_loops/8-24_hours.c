52056f9feed05951c2b0686a22feef4dc55de229
y-gom3a <yosifgomaa2002@gmail.com>
2023-03-21 18:35:18 -0700

    fg

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,9 +18,9 @@ for (d = 0 ; d <= 9 ; d++)
 if (a >= 2 && b >= 4)
 break;
 _putchar(a + 48);
--putchar(b + 48);
--putchar (58);
--putchar(c + 48);
+_putchar(b + 48);
+_putchar (58);
+_putchar(c + 48);
 _putchar(d + 48);
 _putchar('\n');
 }
