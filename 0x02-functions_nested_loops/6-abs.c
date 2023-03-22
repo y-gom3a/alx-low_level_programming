ff3cf4b7b32623ad33eee48b3fdbc0e497bcd23a
y-gom3a <yosifgomaa2002@gmail.com>
2023-03-21 18:07:15 -0700

    dff

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - is my main function
+ *@i : input intger  number
+ * Return: Always 0 (Success)
+ */
+int _abs(int i)
+{
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (-1 * i);
+}
+}
